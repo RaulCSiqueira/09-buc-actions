9f7db5489140f68175f654a0e2f98355cfbb50e6
Raul C Siqueira <raulcesarsiqueira@gmail.com>
2024-04-12 21:26:43 -0300

    teste 3

--- .github/actions/deplot-s3-javascript/main.js
+++ .github/actions/deplot-s3-javascript/main.js
@@ -9,8 +9,8 @@ function run() {
   const distFolder = core.getInput('dist-folder', { required: true });
 
   // 2) Uplaod files
-  const s3Uri = 's3://${bucket}';
-  exec.exec('aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}');
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
   const websiteUrl = 'http://${bucket}.s3-website-${bucketRegion}.amazonaws.com';
   core.setOutput('website-url', websiteUrl); // ::set-output
